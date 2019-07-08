88306a71be48668c0bace2b7c6eea475ebbfc384
Moses Hassan <mosesahassan@gmail.com>
2019-07-08 19:05:20 +0700

    Updated pipeline code

--- packages/regression_model/pipeline.py
+++ packages/regression_model/pipeline.py
@@ -1,7 +1,34 @@
+from sklean.linear_model import Lasso
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 import preprocessors as pp
 
+
+# categorical variables with NA in train set
+CATEGORICAL_VARS_WITH_NA = [
+    "MasVnrType",
+    "BsmtQual",
+    "BsmtExposure",
+    "FireplaceQu",
+    "GarageType",
+    "GarageFinish",
+]
+
+TEMPORAL_VARS = "YearRemodAdd"
+
+# this variable is to calculate the temporal variable,
+# can be dropped afterwards
+DROP_FEATURES = "YrsSold"
+
+# variables to log transform
+
+NUMERICAL_LOG_VARS = ["LotFrontge", "1stFlrSF", "GrLivArea"]
+
+# numerical variables with NA in train set
+NUMERICAL_VARS_WITH_NA = ["LotFrontage"]
+
+# categorical variables to encode
 CATEGORICAL_VARS = [
     "MSZoning",
     "Neighbourhood",
@@ -18,8 +45,28 @@ CATEGORICAL_VARS = [
     "PavedDrive",
 ]
 
-PIPELINE_NAME = "lasso_regression"
 
 price_pipe = Pipeline(
-    [("categorical_imputer", pp.CategorialImputer(variables=CATEGORICAL_VARS))]
+    [
+        (
+            "categorical_imputer",
+            pp.CategorialImputer(variables=CATEGORICAL_VARS_WITH_NA),
+        ),
+        ("numerical_imputer", pp.NumericalImputer(vairbales=NUMERICAL_VARS_WITH_NA)),
+        (
+            "temporal_variable",
+            pp.TEMPORAL_VARIABLE_ESTIMATOR(
+                variables=TEMPORAL_VARS, reference_variable=TEMPORAL_VARS
+            ),
+        ),
+        (
+            "rare_label_encoder",
+            pp.RareLabelCategoricalEncoder(tol=0.01, variables=CATEGORICAL_VARS),
+        ),
+        ("categorical_encoder", pp.CategoricalEncoder(variables=NUMERICAL_LOG_VARS)),
+        ("log_transformer", pp.LogTransformer(variables=NUMERICAL_LOG_VARS)),
+        ("drop_features", pp.DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+        ("scaler", MinMaxScaler()),
+        ("Linear_model", Lassor(alpha=0.005, random_state=0)),
+    ]
 )
