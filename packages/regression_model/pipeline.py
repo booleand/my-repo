56d4e5df1e8374aa923fc5d296ee6ff0833542ec
Moses Hassan <mosesahassan@gmail.com>
2019-07-17 21:42:15 +0700

    updated pipeline.py

--- packages/regression_model/pipeline.py
+++ packages/regression_model/pipeline.py
@@ -1,8 +1,8 @@
-from sklean.linear_model import Lasso
+from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
-import preprocessors as pp
+from regression_model import preprocessors as pp
 
 
 # categorical variables with NA in train set
@@ -19,11 +19,10 @@ TEMPORAL_VARS = "YearRemodAdd"
 
 # this variable is to calculate the temporal variable,
 # can be dropped afterwards
-DROP_FEATURES = "YrsSold"
+DROP_FEATURES = "YrSold"
 
 # variables to log transform
-
-NUMERICAL_LOG_VARS = ["LotFrontge", "1stFlrSF", "GrLivArea"]
+NUMERICALS_LOG_VARS = ["LotFrontage", "1stFlrSF", "GrLivArea"]
 
 # numerical variables with NA in train set
 NUMERICAL_VARS_WITH_NA = ["LotFrontage"]
@@ -31,7 +30,7 @@ NUMERICAL_VARS_WITH_NA = ["LotFrontage"]
 # categorical variables to encode
 CATEGORICAL_VARS = [
     "MSZoning",
-    "Neighbourhood",
+    "Neighborhood",
     "RoofStyle",
     "MasVnrType",
     "BsmtQual",
@@ -50,23 +49,23 @@ price_pipe = Pipeline(
     [
         (
             "categorical_imputer",
-            pp.CategorialImputer(variables=CATEGORICAL_VARS_WITH_NA),
+            pp.CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA),
         ),
-        ("numerical_imputer", pp.NumericalImputer(vairbales=NUMERICAL_VARS_WITH_NA)),
+        ("numerical_imputer", pp.NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
         (
             "temporal_variable",
-            pp.TEMPORAL_VARIABLE_ESTIMATOR(
-                variables=TEMPORAL_VARS, reference_variable=TEMPORAL_VARS
+            pp.TemporalVariableEstimator(
+                variables=TEMPORAL_VARS, reference_variable=DROP_FEATURES
             ),
         ),
         (
             "rare_label_encoder",
             pp.RareLabelCategoricalEncoder(tol=0.01, variables=CATEGORICAL_VARS),
         ),
-        ("categorical_encoder", pp.CategoricalEncoder(variables=NUMERICAL_LOG_VARS)),
-        ("log_transformer", pp.LogTransformer(variables=NUMERICAL_LOG_VARS)),
+        ("categorical_encoder", pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
+        ("log_transformer", pp.LogTransformer(variables=NUMERICALS_LOG_VARS)),
         ("drop_features", pp.DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
         ("scaler", MinMaxScaler()),
-        ("Linear_model", Lassor(alpha=0.005, random_state=0)),
+        ("Linear_model", Lasso(alpha=0.005, random_state=0)),
     ]
 )
