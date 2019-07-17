66f95149f9195af1de8672402ab7e1720de68407
Moses Hassan <mosesahassan@gmail.com>
2019-07-17 19:09:55 +0700

    updated training pipeline

--- packages/regression_model/train_pipeline.py
+++ packages/regression_model/train_pipeline.py
@@ -1,20 +1,28 @@
 import pathlib
 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.externals import joblib
+
+import pipeline
+
+
 PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
-
-TESTING_DATA_FILE = DATASET_DIR / "test_csv"
-TRAINING_DATA_FILE = DATASET_DIR / "trainging.csv"
+TESTING_DATA_FILE = DATASET_DIR / "test.csv"
+TRAINING_DATA_FILE = DATASET_DIR / "train.csv"
 TARGET = "SalePrice"
 
+
 FEATURES = [
-    "MSSubclass",
+    "MSSubClass",
     "MSZoning",
     "Neighborhood",
     "OverallQual",
     "OverallCond",
-    "YearRemoAdd",
+    "YearRemodAdd",
     "RoofStyle",
     "MasVnrType",
     "BsmtQual",
@@ -37,16 +45,34 @@ FEATURES = [
 ]
 
 
-def save_pipeline() -> None:
-    """ Persist the pipeline."""
+def save_pipeline(*, pipeline_to_persist) -> None:
+    """Persist the pipeline."""
 
-    pass
+    save_file_name = "regression_model.pkl"
+    save_path = TRAINED_MODEL_DIR / save_file_name
+    joblib.dump(pipeline_to_persist, save_path)
+
+    print("saved pipeline")
 
 
 def run_training() -> None:
-    """ Train the model."""
+    """Train the model."""
 
-    print("Training...")
+    # read training data
+    data = pd.read_csv(TRAINING_DATA_FILE)
+
+    # divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[FEATURES], data[TARGET], test_size=0.1, random_state=0
+    )  # we are setting the seed here
+
+    # transform the target
+    y_train = np.log(y_train)
+    y_test = np.log(y_test)
+
+    pipeline.price_pipe.fit(X_train[FEATURES], y_train)
+
+    save_pipeline(pipeline_to_persist=pipeline.price_pipe)
 
 
 if __name__ == "__main__":
